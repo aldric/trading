f41c6f7b10e5ab72847f285555bd70b245381dc2
a.gaudinot <gaudinot@gmail.com>
2016-08-03 08:52:57 +0200

    Slick slider configuration

--- wp-content/themes/tuto/content-page.php
+++ wp-content/themes/tuto/content-page.php
@@ -1,6 +1,6 @@
-<?php /* Default template for displaying page content */ ?>
-<article id="page-<?php the_ID(); ?>" <?php post_class('mh-content-wrapper'); ?>>
-	<div class="entry-content clearfix">
-		<?php the_content(); ?>
-	</div>
+<?php /* Default template for displaying page content */ ?>
+<article id="page-<?php the_ID(); ?>" <?php post_class('mh-content-wrapper'); ?>>
+	<div class="entry-content clearfix">
+		<?php the_content(); ?>
+	</div>
 </article>
--- wp-content/w3tc-config/master-admin.php
+++ wp-content/w3tc-config/master-admin.php
@@ -28,5 +28,5 @@ return array(
 	'notes.remove_w3tc' => false,
 	'notes.hide_extensions' => array(
 	),
-	'evaluation.reminder' => 1468592048,
+	'evaluation.reminder' => 1472319197,
 );
